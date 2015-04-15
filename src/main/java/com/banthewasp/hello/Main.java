cdad7f1469492412b84f26d6ae6abffc3c3826ce
p&k <p&k@flarp2>
2015-04-15 21:28:40 +0100

    First code added.

---
+++ src/main/java/com/banthewasp/hello/Main.java
@@ -0,0 +1,11 @@
+package com.banthewasp.hello;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+	System.out.println("Hello world.");
+    }
+}
